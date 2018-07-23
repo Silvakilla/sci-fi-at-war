22b05a9ccb6f3822cded1bb65776cc50453b73f2
Silvakilla <goldkilla34@gmail.com>
2018-07-23 17:16:24 +0200

    Romulan balance patch

--- copy.bat
+++ copy.bat
@@ -1,3 +1,4 @@
 echo "Start copy"
 xcopy /s /y /a "e:\modding\sci-fi-at-war\xml"  "e:\SteamLibrary\steamapps\common\Star Wars Empire at War\corruption\Mods\swsfawse_development\data\xml"
+xcopy /s /y /a "e:\modding\sci-fi-at-war\text" "e:\SteamLibrary\steamapps\common\Star Wars Empire at War\corruption\Mods\swsfawse_development\data\text"
 echo "Copy finished"
