ab74c20021e74222ce2ed268056de4fae50e983b
Silvakilla <goldkilla34@gmail.com>
2018-07-23 14:26:24 +0200

    Release of Test Version 11

---
+++ copy.bat
@@ -0,0 +1,3 @@
+echo "Start copy"
+xcopy /s /y /a "e:\modding\sci-fi-at-war\xml"  "e:\SteamLibrary\steamapps\common\Star Wars Empire at War\corruption\Mods\swsfawse_development\data\xml"
+echo "Copy finished"
