785be96ca72c1824838ea2c0b9ed1da776f8bbf1
Silvakilla <goldkilla34@gmail.com>
2018-07-26 11:53:22 +0200

    last changes before MR

--- copy.bat
+++ copy.bat
@@ -1,4 +1,5 @@
 echo "Start copy"
 xcopy /s /y /a "e:\modding\sci-fi-at-war\xml"  "e:\SteamLibrary\steamapps\common\Star Wars Empire at War\corruption\Mods\swsfawse_development\data\xml"
 xcopy /s /y /a "e:\modding\sci-fi-at-war\text" "e:\SteamLibrary\steamapps\common\Star Wars Empire at War\corruption\Mods\swsfawse_development\data\text"
+xcopy /s /y /a "e:\modding\sci-fi-at-war\resources" "e:\SteamLibrary\steamapps\common\Star Wars Empire at War\corruption\Mods\swsfawse_development\data\resources"
 echo "Copy finished"
