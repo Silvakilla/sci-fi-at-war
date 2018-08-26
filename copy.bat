e171fe5f5176983661ebd6ccbc4a11629fd5c00f
Silvakilla <goldkilla34@gmail.com>
2018-08-26 20:56:38 +0200

    Added Halo Ground Units, Added Covenant Space Units

--- copy.bat
+++ copy.bat
@@ -3,3 +3,4 @@ xcopy /s /y /a "e:\modding\sci-fi-at-war\xml"  "e:\SteamLibrary\steamapps\common
 xcopy /s /y /a "e:\modding\sci-fi-at-war\text" "e:\SteamLibrary\steamapps\common\Star Wars Empire at War\corruption\Mods\swsfawse_development\data\text"
 xcopy /s /y /a "e:\modding\sci-fi-at-war\resources" "e:\SteamLibrary\steamapps\common\Star Wars Empire at War\corruption\Mods\swsfawse_development\data\resources"
 echo "Copy finished"
+echo %TIME%
